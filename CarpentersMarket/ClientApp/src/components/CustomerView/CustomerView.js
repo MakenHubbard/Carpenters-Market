be2a061f60672029332840f6e70f76ac24076203
Maken Hubbard <makhub26@outlook.com>
2019-02-04 20:39:53 -0600

    have the get carpenter working in the customerview component

---
+++ CarpentersMarket/ClientApp/src/components/CustomerView/CustomerView.js
@@ -0,0 +1,60 @@
+﻿import React from 'react';
+import { Link } from 'react-router-dom';
+
+import usersRequests from '../../ApiCalls/UserRequests';
+
+
+class CustomerView extends React.Component {
+    state = {
+        users: [],
+    }
+
+    componentDidMount = (e) => {
+        this.getAllUsers();
+    }
+
+    getAllUsers = () => {
+        usersRequests
+            .getAllUsers()
+            .then((users) => {
+                this.setState({ users: users });
+            })
+            .catch((err) => {
+                console.error('in the customerview component', err);
+            })
+    }
+
+    render() {
+
+        const carpenters = this.state.users.map(user => {
+            if (user.isCarpenter == true) {
+                return (
+                    <div key={user.id} className="panel panel-default" >
+                        <div className="panel-heading">
+                            <h3 className="panel-title">Carpenter Id: {user.id}</h3>
+                        </div>
+                        <div className="panel-body">
+                            <p>Carpenter: {`${user.firstName}` + " " + `${user.lastName}`}</p>
+
+                        </div>
+                    </div >
+                )
+            }
+        })
+
+        return (
+            <div className="carpentersData">
+                <div>
+                    <p><Link to='/' className='btn btn-lg btn-success'>Back to Home</Link></p>
+                </div>
+                <div>
+                    {carpenters}
+                </div>
+            </div>
+        )
+    }
+
+
+}
+
+export default CustomerView;
---
+++ CarpentersMarket/Controllers/UsersContorller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarpentersMarket.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace CarpentersMarket.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UsersStorage _storage;
+
+        public UsersController(IConfiguration config)
+        {
+            _storage = new UsersStorage(config);
+        }
+
+        [HttpGet]
+        public IActionResult GetAllUsers ()
+        {
+            return Ok(_storage.GetAll());
+        }
+    }
+}
