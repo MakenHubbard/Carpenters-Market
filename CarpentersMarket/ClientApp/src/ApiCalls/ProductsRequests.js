7a347f74e6ac3b7becda4ce7341324afa7107698
Maken Hubbard <makhub26@outlook.com>
2019-02-04 21:30:02 -0600

    adding a wip on product things

---
+++ CarpentersMarket/ClientApp/src/ApiCalls/ProductsRequests.js
@@ -0,0 +1,16 @@
+﻿import axios from 'axios';
+
+const getAllProducts = () => {
+    return new Promise((resolve, reject) => {
+        axios
+            .get('api/products')
+            .then(res => {
+                resolve(res.data);
+            })
+            .catch(err => {
+                reject(err);
+            });
+    });
+};
+
+export default { getAllProducts };
---
+++ CarpentersMarket/Models/Products.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarpentersMarket.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int UsersId { get; set; }
+        public int ProductTypeId { get; set; }
+        public int Quantity { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
